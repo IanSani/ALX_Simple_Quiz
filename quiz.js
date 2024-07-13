544b73f8bbfec237bf8540c4872145ce33a51d0c
iansani <iansani259@gmail.com>
2024-07-13 19:53:44 +0300

    Added event listener

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer() {
+    let correctAnswer="4";
+    let checkedRadioButton=document.querySelector('input[name="quiz"]:checked');
+    let userAnswer=checkedRadioButton? checkedRadioButton.value : null;
+    let feedback=document.getElementById('feedback');
+    if(userAnswer===correctAnswer){
+        feedback.textContent="Correct! Well done.";
+    }else{
+        feedback.textContent="That's incorrect. Try again!";
+    }
+}
+let submitAnswer=document.getElementById('submit-answer');
+submitAnswer.addEventListener('click',checkAnswer);
