fe5d0619b9dcb71a387f1e6267a94ca2c3690870
iansani <iansani259@gmail.com>
2024-07-13 19:59:42 +0300

    Made more changes

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     let checkedRadioButton=document.querySelector('input[name="quiz"]:checked');
     let userAnswer=checkedRadioButton? checkedRadioButton.value : null;
     let feedback=document.getElementById('feedback');
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         feedback.textContent="Correct! Well done.";
     }else{
         feedback.textContent="That's incorrect. Try again!";
